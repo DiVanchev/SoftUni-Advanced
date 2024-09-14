3cdb82d0a47661cf6ce7730d1135387c45d7dd83
Dimitar Vachev <d.vachev2002@gmail.com>
2024-09-14 11:59:54 +0300

    spelling fix

--- Syntax, Functions and Statements/L - Syntax, Functions and Statements/aggregateElements.js
+++ Syntax, Functions and Statements/L - Syntax, Functions and Statements/aggregateElements.js
@@ -1,19 +1,19 @@
 function aggregateElements(arr) {
   let sum = 0;
-  let inversSum = 0;
+  let inverseSum = 0;
   let concatenate = '';
   for (const num of arr) {
     sum += num;
   }
   for (let i = 0; i < arr.length; i++) {
-    inversSum += 1 / arr[i];
+    inverseSum += 1 / arr[i];
   }
   for (const concat of arr) {
    let stringifiedConcat = String(concat);
    concatenate += stringifiedConcat;
   }
   console.log(sum);
-  console.log(inversSum);
+  console.log(inverseSum);
   console.log(concatenate);
   
 }
