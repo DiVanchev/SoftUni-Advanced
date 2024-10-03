4dd07cedd93ac5a8d580f55ef6629e72d4ebef79
Dimitar Vachev <d.vachev2002@gmail.com>
2024-10-04 00:38:47 +0300

    up

--- DOM/E DOM/02. Pascal or Camel Case/solution.js
+++ DOM/E DOM/02. Pascal or Camel Case/solution.js
@@ -1,18 +1,29 @@
 function solve() {
-  const input = document.querySelector('#text').value.toLowerCase().split(' ');
-  const conversion = document.querySelector('#naming-convention').value.toLowerCase();
-  const resultEl = document.querySelector('#result');
+  const input = document.querySelector("#text").value.toLowerCase().split(" ");
+  const conversion = document
+    .querySelector("#naming-convention")
+    .value.toLowerCase();
+  const resultEl = document.querySelector("#result");
 
-  if (input == '' || conversion == '') return;
+  if (input == "" || conversion == "") {
+    return;
+  }
 
-  let result = '';
+  let result = "";
 
-  if(conversion == 'camel case') {
-    result = input[0] + input.slice(1).map((word) => word[0].toUpperCase() + word.slice(1)).join(''));
-  } else if (conversion == 'pascal case') {
-    result = '';
+  if (conversion == "camel case") {
+    result =
+      input[0] +
+      input
+        .slice(1)
+        .map((word) => word[0].toUpperCase() + word.slice(1))
+        .join("");
+  } else if (conversion == "pascal case") {
+    result = input
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join("");
   } else {
-    result = 'Error!';
+    result = "Error!";
   }
 
   resultEl.textContent = result;
--- DOM/E DOM/03. Accordion/accordion.js
+++ DOM/E DOM/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+  const buttonEl = document.querySelector(".button");
+  const extraEL = document.querySelector("#extra");
+  const initialDisplay = extraEL.style.display;
+
+  if (initialDisplay == "block") {
+    extraEL.style.display = "none";
+    buttonEl.textContent = "More";
+  } else {
+    extraEL.style.display = "block";
+    buttonEl.textContent = "Less";
+  }
 }
